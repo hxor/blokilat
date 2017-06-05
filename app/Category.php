431f476b3d9ed44b673dbd11662654812f6ec709
hxor <4kiddie04@gmail.com>
2017-06-06 01:13:57 +0700

    add relation model

--- app/Category.php
+++ app/Category.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $fillable = ['category'];
+
+    /**
+     * Sebuah kategori memiliki banyak postingan
+     * @return [type] [description]
+     */
+    public function posts(){
+    	return $this->hasMany('App\Posts');
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['user_id', 'category_id', 'title', 'body', 'status'];
+
+    /**
+     * Sebuah Post data dimiliki satu user
+     * @return [type] [description]
+     */
+    public function user(){
+    	return $this->belongsTo('App\User');
+    }
+
+    /**
+     * Sebuah Post data dimiliki satu category
+     * @return [type] [description]
+     */
+    public function category(){
+    	return $this->belongsTo('App\Category');
+    }
 }
